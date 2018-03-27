d3f90f2827ff11e8633501cc119356432f98e83b
Eduardo Cervantes <eddyizm@gmail.com>
2018-03-27 14:49:13 -0700

    added mock data and using offline mode.

---
+++ basicOverLayAreaChart.py
@@ -0,0 +1,17 @@
+# basic overlaid area chart
+import plotly.offline as py
+import plotly.graph_objs as go
+
+trace1 = go.Scatter(
+    x=[1, 2, 3, 4, 6, 7, 8, 9, 10, 12],
+    y=[2749 ,2552 ,5424 ,2647 ,1552 ,342 ,331 ,309 ,650 ,299],
+    fill='tozeroy'
+)
+# trace2 = go.Scatter(
+#     x=[1, 2, 3, 4],
+#     y=[3, 5, 1, 7],
+#     fill='tonexty'
+# )
+data = [trace1]
+py.offline.plot(data, filename='basic-area')
+
