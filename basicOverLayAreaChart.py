6a01c2cfaf01b5f692906a26a5a356b9ddd9dfb7
Eduardo Cervantes <eddyizm@gmail.com>
2018-03-27 15:53:59 -0700

    figured out how to export image file programmaticaly

--- basicOverLayAreaChart.py
+++ basicOverLayAreaChart.py
@@ -2,11 +2,22 @@
 import plotly.offline as py
 import plotly.graph_objs as go
 
-trace1 = go.Scatter(
+trace1 = go.Bar(
     x=[1, 2, 3, 4, 6, 7, 8, 9, 10, 12],
     y=[2749 ,2552 ,5424 ,2647 ,1552 ,342 ,331 ,309 ,650 ,299],
-    fill='tozeroy',
-    
+    #fill='tozeroy',
+    #fillcolor='green',
+    # name = 'Above',
+    # mode = 'markers',
+    # marker = dict(
+    #     size = 10,
+    #     color = 'rgba(152, 0, 0, .8)',
+    #     line = dict(
+    #         width = 2,
+    #         color = 'rgb(0, 0, 0)'
+    #     )
+    #)
+        
 )
 # trace2 = go.Scatter(
 #     x=[1, 2, 3, 4],
@@ -14,5 +25,7 @@ trace1 = go.Scatter(
 #     fill='tonexty'
 # )
 data = [trace1]
-py.offline.plot(data, filename='basic-area', link_text="Aetna 2015")
+#py.offline.plot(data, filename='basic-bar')
+'''this method allows you to export image file while generating the html file. '''
+py.plot(data, image='png')
 
