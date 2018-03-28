d9b31ab7483740e0c6298345139321006c03926a
Eduardo Cervantes <eddyizm@gmail.com>
2018-03-28 16:46:19 -0700

    playing with wxpython

--- basicOverLayAreaChart.py
+++ basicOverLayAreaChart.py
@@ -2,11 +2,11 @@
 import plotly.offline as py
 import plotly.graph_objs as go
 
-trace1 = go.Bar(
+trace1 = go.Scatter(
     x=[1, 2, 3, 4, 6, 7, 8, 9, 10, 12],
     y=[2749 ,2552 ,5424 ,2647 ,1552 ,342 ,331 ,309 ,650 ,299],
-    #fill='tozeroy',
-    #fillcolor='green',
+    fill='tozeroy',
+    fillcolor='green',
     # name = 'Above',
     # mode = 'markers',
     # marker = dict(
@@ -25,7 +25,7 @@ trace1 = go.Bar(
 #     fill='tonexty'
 # )
 data = [trace1]
-#py.offline.plot(data, filename='basic-bar')
+py.offline.plot(data, filename='basic-area.html', image='png')
 '''this method allows you to export image file while generating the html file. '''
-py.plot(data, image='png')
+#py.plot(data, image='png')
 
